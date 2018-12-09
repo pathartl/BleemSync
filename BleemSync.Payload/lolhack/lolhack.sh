443ddae66c9ecbadbc4d392a174db523150a9cf7
Pat Hartl <pat@pathar.tl>
2018-12-09 03:09:23 -0600

    Started adding partial support for moving saves off system and keeping them on the USB drive

--- BleemSync.Payload/lolhack/lolhack.sh
+++ BleemSync.Payload/lolhack/lolhack.sh
@@ -7,6 +7,7 @@ mkdir -p /media/System/Bios
 mkdir -p /media/System/Preferences
 mkdir -p /media/System/Databases
 mkdir -p /media/System/Region
+mkdir -p /media/System/UI
 
 killall ui_menu
 
@@ -34,6 +35,9 @@ fi
 
 killall ui_menu
 
+# Allow access to the PCSX menu via Select + Triangle
+export PCSX_ESC_KEY=2
+
 # Safely overmount the games and user data folder
 source /media/System/MountGames.sh
 # find /media/Games -maxdepth 1 -type d -printf "%f\n" > /media/System/payload.log
@@ -46,11 +50,34 @@ source /media/System/MountGames.sh
 # 	fi
 # done
 
-# mount -o bind /media/System/Bios /gaadata/system/bios
-# mount -o bind /media/System/Preferences /gaadata/preferences
+# Overmount some files
+#mount -o bind /media/System/sonyapp-copy /usr/sony/bin/sonyapp-copy
+#mount -o bind /media/System/sonyapp-copylink /usr/sony/bin/sonyapp-copylink
+mount -o bind /media/System/bin /usr/sony/bin
+
+# Overmount some folders
+mount -o bind /media/System/Bios /gaadata/system/bios
+mount -o bind /media/System/Preferences /gaadata/preferences
 mount -o bind /media/System/Databases /gaadata/databases
-# mount -o bind /media/System/Region /gaadata/geninfo
-# mount -o bind /media/UserData /data/AppData
+mount -o bind /media/System/Region /gaadata/geninfo
+mount -o bind /media/System/UI /data/sony/ui
+#mount -o bind /media/UserData /data/AppData
+
+# The pcsx.cfg file needs to be copied into the user data folder or controllers may not work.
+cd /media/Games
+find * -type d -maxdepth 1 -exec mkdir -p /media/UserData/{} \;
+find * -type d -maxdepth 1 -exec mount -o bind /media/UserData/{} /data/AppData/sony/pcsx/{} \;
+find * -type d -maxdepth 1 -exec mkdir -p /data/AppData/sony/pcsx/{}/.pcsx \;
+find * -type d -maxdepth 1 -exec cp /media/Games/{}/pcsx.cfg /data/AppData/sony/pcsx/{}/.pcsx/pcsx.cfg \;
+cd -
+
+mkdir -p /data/AppData/sony/pcsx/bios
+cp /media/System/Bios/* /data/AppData/sony/pcsx/bios
+
+mkdir -p /data/AppData/sony/pcsx/plugins
+cp /usr/sony/bin/plugins/* /data/AppData/sony/pcsx/plugins
+
+/usr/sony/bin/sonyapp-copy
 
 killall ui_menu
 
