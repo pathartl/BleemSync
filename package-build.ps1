2d7012a161d3ce69c8dc7e568ad62231df5a0608
Pat Hartl <pat@pathar.tl>
2018-12-13 21:13:34 -0600

    Switch to Compress-7Zip for better cross-platform compatible ZIPs

--- package-build.ps1
+++ package-build.ps1
@@ -1,12 +1,15 @@
 $version = "0.3.2"
 
+# Must run the following as admin if you want to build and compress
+# Install-Module 7Zip4PowerShell -Force -Verbose
+
 dotnet publish -c release -r linux-x64
 rm -Recurse -Force .\Publish
 mkdir .\Publish
 mkdir .\Publish\BleemSync
 cp .\BleemSync\bin\Release\netcoreapp2.1\linux-x64\publish\* .\Publish\BleemSync
 cp -Recurse .\BleemSync.Payload\* .\Publish
-Compress-Archive -Force -Path .\Publish\* -CompressionLevel Optimal -DestinationPath .\BleemSync-$version-linux-x64.zip
+Compress-7Zip "Publish\*" -ArchiveFileName BleemSync-$version-linux-x64.zip -Format Zip
 
 dotnet publish -c release -r osx-x64
 rm -Recurse -Force .\Publish
@@ -14,7 +17,7 @@ mkdir .\Publish
 mkdir .\Publish\BleemSync
 cp .\BleemSync\bin\Release\netcoreapp2.1\osx-x64\publish\* .\Publish\BleemSync
 cp -Recurse .\BleemSync.Payload\* .\Publish
-Compress-Archive -Force -Path .\Publish\* -CompressionLevel Optimal -DestinationPath .\BleemSync-$version-osx-x64.zip
+Compress-7Zip "Publish" -ArchiveFileName BleemSync-$version-osx-x64.zip -Format Zip
 
 dotnet publish -c release -r win7-x86
 rm -Recurse -Force .\Publish
@@ -22,4 +25,4 @@ mkdir .\Publish
 mkdir .\Publish\BleemSync
 cp .\BleemSync\bin\Release\netcoreapp2.1\win7-x86\publish\* .\Publish\BleemSync
 cp -Recurse .\BleemSync.Payload\* .\Publish
-Compress-Archive -Force -Path .\Publish\* -CompressionLevel Optimal -DestinationPath .\BleemSync-$version-win7-x86.zip
+Compress-7Zip "Publish" -ArchiveFileName BleemSync-$version-win7-x86.zip -Format Zip
