a36549503a2d61113e44af816f9feb95c8fb9695
Pat Hartl <pat@pathar.tl>
2019-01-06 19:50:41 -0600

    Started moving some functionality for reading files to JavaScript workers to take advantage of FileReaderAsync to retain sanity

--- BleemSync/wwwroot/lib/bleemsync/game-manager.js
+++ BleemSync/wwwroot/lib/bleemsync/game-manager.js
@@ -4,12 +4,16 @@
         this._forms = $('.game-manager-form');
         this._editGameForm = $('#edit-game-form');
         this._addGameForm = $('#add-game-form');
+        this._uploadInput = $('input[type="file"]');
 
         this.Init();
     }
 
     Init() {
         $.getJSON('/Games/GetTree', (data) => this.InitTree(data));
+
+        this._uploadInput.change(() => this.ParseUploader());
+        this.LoadAddGameForm();
     }
 
     InitTree(data) {
@@ -67,4 +71,91 @@
         this._forms.hide();
         this._addGameForm.show().setViewModel(viewModel);
     }
+
+    ReadFileAsText(file) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+
+            reader.onload = function (event) {
+                resolve(event.target.result);
+            };
+
+            reader.onerror = reject;
+
+            reader.readAsText(file);
+        });
+    }
+
+    ParseUploader() {
+        var uploader = this._uploadInput.get(0);
+        var worker = new Worker('/lib/bleemsync/scrape-games.js');
+
+        worker.postMessage(uploader.files);
+
+        var parsingPromises = [];
+
+        for (let file of uploader.files) {
+            switch (file.name.split('.').pop()) {
+                case 'cue':
+                    //parsingPromises.push(this.ParseCueSheet(file, uploader.files));
+                    break;
+            }
+        }
+
+        var gameResults = Promise.all(parsingPromises).then((games) => {
+            console.log(games);
+        });
+    }
+
+    ParseCueSheet(cueSheet, allFiles) {
+        let contentPromise = this.ReadFileAsText(cueSheet);
+        /*
+        return new Promise((resolve, reject) => {
+            var reader = new FileReader();
+            
+            reader.onerror = () => {
+                reader.abort();
+                reject(new DOMException("There was a problem parsing the cue sheet as a file."));
+            };
+
+            reader.onload = () => {
+                var binFiles = [];
+
+                var regex = /FILE \"(.+)\" BINARY/m;
+                var matches = reader.result.match(regex);
+
+                for (var match of matches) {
+                    binFiles.push(match);
+                }
+
+                if (binFiles.length == 0) {
+                    reject(new DOMException("No reference to any .bin files was found."));
+                } else {
+                    var binFilesFound = [];
+
+                    for (var binFile of binFiles) {
+                        var foundFile = false;
+
+                        for (var file of allFiles) {
+                            if (file.name == binFile && !foundFile) {
+                                binFilesFound.push(file);
+                                foundFile = true;
+                            }
+                        }
+                    }
+
+                    if (binFilesFound.length === binFiles.length) {
+                        resolve(binFiles);
+                    } else {
+                        reject(new DOMException("You must upload all .bin files associated with the cue sheet."));
+                    }
+                }
+
+                reader.readAsText(cueSheet);
+            };
+        });
+        */
+    }
+
+
 }
---
+++ BleemSync/wwwroot/lib/bleemsync/scrape-games.js
@@ -0,0 +1,135 @@
+﻿class ParserResponse {
+    constructor() {
+        this.Game = null;
+        this.Valid = true;
+        this.Message = "";
+    }
+}
+
+onmessage = function (e) {
+    let reader = new FileReaderSync();
+    let files = e.data;
+
+    for (let file of files) {
+        let extension = file.name.split('.').pop();
+        let games = [];
+
+        switch (extension) {
+            case 'cue':
+                games.push(ParseCueFile(file, files));
+        }
+    }
+    let text = reader.readAsText(e.data[0]);
+
+    function ParseCueFile(file, files) {
+        // Need all files so we can verify all parts of the .cue exist
+        let response = new ParserResponse();
+        let regex = /FILE \"(.+)\" BINARY/gm;
+        let binFiles = [];
+
+        let reader = new FileReaderSync();
+        let contents = reader.readAsText(file);
+
+        while (match = regex.exec(contents)) {
+            binFiles.push(match[1]);
+        }
+
+        if (binFiles.length == 0) {
+            response.Valid = false;
+            response.Message = "The cue sheet is incorrect or corrupted. No reference to binary files was found."
+        } else {
+            var binFilesFound = [];
+
+            for (var binFile of binFiles) {
+                var foundFile = false;
+
+                for (let file of files) {
+                    if (file.name == binFile && !foundFile) {
+                        binFilesFound.push(file);
+                        foundFile = true;
+                    }
+                }
+            }
+
+            if (binFilesFound.length === binFiles.length) {
+                response.Valid = true;
+                response.Game = GetGameInfoFromBinaryFiles(binFilesFound);
+            } else {
+                response.Valid = false;
+                response.Message = "Not all binary files were selected, or they are misnamed."
+            }
+        }
+    }
+
+    function GetGameInfoFromBinaryFiles(files) {
+        var fingerprint = "";
+
+        fingerprint = GetPlayStationFingerprint(files[0]);
+    }
+
+    function GetPlayStationFingerprint(file) {
+        var serial = "";
+        var foundPossibleString = false;
+        var foundSerial = false;
+
+        var serialNumberPrefixes = [
+            "CPCS",
+            "ESPM",
+            "HPS",
+            "LPS",
+            "LSP",
+            "SCAJ",
+            "SCED",
+            "SCES",
+            "SCPS",
+            "SCUS",
+            "SIPS",
+            "SLES",
+            "SLKA",
+            "SLPM",
+            "SLPS",
+            "SLUS"
+        ];
+
+        var bufferSize = 32768;
+        var reader = new FileReaderSync();
+
+        for (let position = 0; position < file.size; position += bufferSize) {
+            let blob = file.slice(position, position + bufferSize);
+            let buffer = new Uint8Array(reader.readAsArrayBuffer(blob));
+            let bufferStr = String.fromCharCode.apply(null, buffer);
+
+            for (let prefix of serialNumberPrefixes) {
+                let prefixIndex = bufferStr.indexOf(prefix);
+
+                if (prefixIndex !== -1) {
+                    let dirtySerial = bufferStr.slice(prefixIndex, prefixIndex + 11);
+                    try {
+                        serial = CleanPlayStationSerial(dirtySerial);
+                    } catch {}
+                    break;
+                }
+            }
+
+            if (serial != '') {
+                break;
+            }
+        }
+
+        return serial;
+    }
+
+    function CleanPlayStationSerial(dirtySerial) {
+        const regex = /([C|E|H|L|S][A-Z]{2,3}).?(\d{2,5}).?(\d+)/gm;
+        m = regex.exec(dirtySerial);
+
+        var cleanSerial = m[1] + '-';
+        var groups = m.length - 1;
+
+        for (var i = 2; i <= groups; i++) {
+            cleanSerial += m[i];
+        }
+
+        return cleanSerial;
+    }
+};
