d687aa2f31c962e22a480dcb938d2571f270cabe
Yukai Li <yukaili.geek@gmail.com>
2019-01-21 01:11:24 -0700

    Target alert timeout

--- BleemSync/wwwroot/lib/bleemsync/alerts.js
+++ BleemSync/wwwroot/lib/bleemsync/alerts.js
@@ -2,7 +2,7 @@
     static Error(message) {
         var alert = $('<div />').attr('class', 'pmd-alert error').text(message);
         $('.pmd-alert-container.center.top').append(alert);
-        setTimeout(AlertService.Clear, 10000);
+        setTimeout(() => alert.remove(), 10000);
     }
 
     static Clear() {
