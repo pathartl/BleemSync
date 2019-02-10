cb0964c80efef060bf0eaf8a107642253d4160c7
Yukai Li <yukaili.geek@gmail.com>
2019-02-09 17:33:36 -0700

    Update game manager service interface definition

--- BleemSync.Services/Abstractions/IGameManagerService.cs
+++ BleemSync.Services/Abstractions/IGameManagerService.cs
@@ -5,7 +5,7 @@ namespace BleemSync.Services.Abstractions
 {
     public interface IGameManagerService
     {
-        void AddGame(GameManagerNode node);
+        void AddGame(GameManagerNode node, bool processFiles = true);
         void UpdateGame(GameManagerNode node);
         void UpdateGames(IEnumerable<GameManagerNode> nodes);
         void DeleteGame(GameManagerNode node);
